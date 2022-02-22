bf265427fcc8f4125d5b0de44cfff3cc2d6253ba
Paul Bouchier <paul.bouchier@gmail.com>
2022-02-22 13:26:57 -0600

    odometry, logs, link working

--- esp_link/scripts/esp_link.py
+++ esp_link/scripts/esp_link.py
@@ -1,65 +1,83 @@
 import time
 import sys
 import rospy
-
 from pySerialTransfer import pySerialTransfer as txfer
 from messages import TxPing, TxDriveMotorsRqst, RxPong, RxOdometry, RxLog
 
 # import a module that will hold objects to be accessed from any module
 sys.path.append('.')
-import config
+#import config
 
-# instantiate message-hander objects
-tx_ping = TxPing()
-tx_drive_motors_rqst = TxDriveMotorsRqst()
-rx_pong = RxPong()
-rx_odometry = RxOdometry()
-rx_log = RxLog()
+# initialize link
+def init_link(port_name):
+    try:
+        global link
+        link = txfer.SerialTransfer(esp_port_name)
+    except:
+        import traceback
+        traceback.print_exc()
+        print("Exception in init_link, exiting")
+        sys.exit()
 
-'''
-list of callback functions to be called during tick. The index of the function
-reference within this list must correspond to the packet ID. For instance, if
-you want to call the function rx_pong() when you parse a packet with an ID of 0, you
-would write the callback list with "rx_pong" being in the 0th place of the list:
-'''
-callback_list = [ rx_pong.handle_pong, rx_odometry.handle_odometry, rx_log.handle_log ]
+# initialize messages
+def init_msgs(link):
+    global tx_ping, rx_pong, rx_odometry, tx_drive_motors_rqst, rx_log
+    tx_ping = TxPing(link)
+    #global rx_pong
+    rx_pong = RxPong(link)
+    rx_odometry = RxOdometry(link)
+    tx_drive_motors_rqst = TxDriveMotorsRqst(link)
+    rx_log = RxLog(link)
+
 
 def ping_callback(event):
     print('Sending ping')
     tx_ping.post()
 
 def drive_motors_callback(event):
+    speed = [50, 50]
+    tx_drive_motors_rqst.post(speed[0], speed[1])
     print('Sending motors')
-    tx_drive_motors_rqst.post(50, 50)
 
 if __name__ == '__main__':
     rospy.init_node('esp_link', disable_signals=True)
-    esp_port_name = rospy.get_param('~esp_port_name', "/dev/ttyUSB0")
-    #esp_port_name = rospy.get_param('~esp_port_name', "/dev/ttyAMA1")
+    #esp_port_name = rospy.get_param('~esp_port_name', "/dev/ttyUSB0")
+    esp_port_name = rospy.get_param('~esp_port_name', "/dev/ttyAMA1")
     rospy.loginfo("esp_link using serial port: {}".format(esp_port_name))
 
     rospy.Timer(rospy.Duration(5.0), ping_callback)
-    rospy.Timer(rospy.Duration(6.0), drive_motors_callback)
+    rospy.Timer(rospy.Duration(0.5), drive_motors_callback)
 
     try:
-        config.link = txfer.SerialTransfer(esp_port_name)
-        config.link.set_callbacks(callback_list)
-        config.link.open()
+        # initialize pySerialTransfer
+        init_link(esp_port_name)
+        init_msgs(link)
+        
+        '''
+        list of callback functions to be called during tick. The index of the function
+        reference within this list must correspond to the packet ID. For instance, if
+        you want to call the function rx_pong() when you parse a packet with an ID of 0, you
+        would write the callback list with "rx_pong" being in the 0th place of the list:
+        '''
+        callback_list = [ rx_pong.handle_pong, rx_odometry.handle_odometry, rx_log.handle_log ]
+
+        link.set_callbacks(callback_list)
+        link.open()
+
         time.sleep(2) # allow some time for the Arduino to completely reset
-        rxMsgCnt = 0;
         
         while True:
-            config.link.tick()
+            link.tick()
 
-            if config.link.status < 0:
-                if config.link.status == txfer.CRC_ERROR:
+            if link.status < 0:
+                if link.status == txfer.CRC_ERROR:
                     print('ERROR: CRC_ERROR')
-                elif config.link.status == txfer.PAYLOAD_ERROR:
+                elif link.status == txfer.PAYLOAD_ERROR:
                     print('ERROR: PAYLOAD_ERROR')
-                elif config.link.status == txfer.STOP_BYTE_ERROR:
+                elif link.status == txfer.STOP_BYTE_ERROR:
                     print('ERROR: STOP_BYTE_ERROR')
                 else:
-                    print('ERROR: {}'.format(config.link.status))
+                    print('ERROR: {}'.format(link.status))
 
             tx_ping.send_posted()
             tx_drive_motors_rqst.send_posted()
@@ -67,7 +85,7 @@ if __name__ == '__main__':
     
     except KeyboardInterrupt:
         try:
-            config.link.close()
+            link.close()
         except:
             pass
     
@@ -76,7 +94,7 @@ if __name__ == '__main__':
         traceback.print_exc()
         
         try:
-            config.link.close()
+            link.close()
         except:
             pass
 
--- esp_link/scripts/messages.py
+++ esp_link/scripts/messages.py
@@ -1,12 +1,13 @@
 import sys
 import rospy
+from time import sleep
 from pySerialTransfer import pySerialTransfer as txfer
 
-sys.path.append('.')
-import config
+from nav_msgs.msg import Odometry
 
 class TxPing:
-    def __init__(self):
+    def __init__(self, link):
+        self.link = link
         self.posted = False
 
     def post(self):
@@ -16,36 +17,85 @@ class TxPing:
         if not self.posted:
             return
         ping_type = '\x00'
-        send_size = config.link.tx_obj(ping_type)
-        config.link.send(send_size, 0)
+        send_size = self.link.tx_obj(ping_type)
+        self.link.send(send_size, 0)
         self.posted = False
 
 class RxPong:
+    def __init__(self, link):
+        self.link = link
+
     def handle_pong(self):
         self.ping_type = b'\x00'
         self.timestamp = 0
-        self.timestamp = config.link.rx_obj(obj_type=type(self.timestamp), obj_byte_size=4, list_format='i')
-        self.pong_type = config.link.rx_obj(obj_type=type(self.ping_type), obj_byte_size=1, list_format='b')
+        self.timestamp = self.link.rx_obj(obj_type=type(self.timestamp), obj_byte_size=4, list_format='i')
+        self.pong_type = self.link.rx_obj(obj_type=type(self.ping_type), obj_byte_size=1, list_format='b')
         rospy.loginfo ("pong type {} timestamp: {}".format(self.pong_type, self.timestamp))
 
 class RxOdometry:
+    def __init__(self, link):
+        self.link = link
+        self.pub = rospy.Publisher('odom', Odometry, queue_size=1)
+        self.odom = Odometry()
+        self.sequence = 0
+        self.log_rate = 50
+        self.log_cnt = 0
+
     def handle_odometry(self):
-        print ('Odometry callback got msg')
+        rospy.logdebug ('Odometry callback got msg length: {}'.format(self.link.bytesRead))
+        self.odom.header.seq = self.sequence
+        self.sequence += 1
+        self.odom.header.stamp = rospy.Time.now()
+        self.odom.header.frame_id = 'base_link'
+        rec_size = 0
+        self.odom.pose.pose.position.x = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        self.odom.pose.pose.position.y = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        heading_rad = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        self.odom.twist.twist.linear.x = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        self.odom.twist.twist.linear.y = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        linear_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        angular_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        odometer = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        left_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        right_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        left_enc_cnt = self.link.rx_obj(obj_type='I', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['I']
+        right_enc_cnt = self.link.rx_obj(obj_type='I', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['I']
+
+        self.log_cnt += 1
+        if (self.log_cnt == self.log_rate):
+            self.log_cnt = 0
+            rospy.loginfo("poseX: {:.2f} poseY: {:.2f} heading: {:.2f} speed {:.2f}, odom: {:.2f}".format(
+                self.odom.pose.pose.position.x, self.odom.pose.pose.position.y, heading_rad, linear_speed, odometer))
+
 
 class RxLog:
+    def __init__(self, link):
+        self.link = link
+
     def handle_log(self):
-        log_length = config.link.bytesRead
-        print (log_length)
+        log_length = self.link.bytesRead
 
         # get the log timestamp
         offset = 0
-        timestamp = config.link.rx_obj(obj_type='i', obj_byte_size=4)
+        timestamp = self.link.rx_obj(obj_type='i', obj_byte_size=4)
 
         # get the log msg, offset 4 bytes from beginning (after timestamp)
         offset += 4
         log_msg = ''
         for i in range(log_length - offset):
-            log_msg += (config.link.rx_obj(obj_type='c', start_pos=i+offset)).decode('utf-8')
+            log_msg += (self.link.rx_obj(obj_type='c', start_pos=i+offset)).decode('utf-8')
         
         # publish the log as a ros log
         timestamp_sec = float(timestamp) / 1000.0
@@ -60,7 +110,8 @@ class RxLog:
         }[level](log_msg + " @ {:.3f}".format(timestamp_sec) + "s")
 
 class TxDriveMotorsRqst:
-    def __init__(self):
+    def __init__(self, link):
+        self.link = link
         self.posted = False
         self.left_drive_pct = 0
         self.right_drive_pct = 0
@@ -68,13 +119,14 @@ class TxDriveMotorsRqst:
     def post(self, left_drive_pct, right_drive_pct):
         self.left_drive_pct = left_drive_pct
         self.right_drive_pct = right_drive_pct
+        rospy.logdebug ("speed_rqst posted: left: {} right: {}".format(self.left_drive_pct, self.right_drive_pct))
         self.posted = True
     
     def send_posted(self):
         if not self.posted:
             return
-        send_size = config.link.tx_obj(self.left_drive_pct)
-        send_size += config.link.tx_obj(self.left_drive_pct, start_pos=send_size)
-        config.link.send(send_size, 1)
+        send_size = self.link.tx_obj(self.left_drive_pct, val_type_override='b')
+        send_size = self.link.tx_obj(self.right_drive_pct, start_pos=send_size, val_type_override='b')
+        self.link.send(send_size, 1)
         self.posted = False
 
