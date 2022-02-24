021370af29fd810be1e48f5875683f73ac1b74f1
Paul Bouchier <paul.bouchier@gmail.com>
2022-02-24 12:48:10 -0600

    add /tf broadcaster, round out odom

--- esp_link/scripts/messages.py
+++ esp_link/scripts/messages.py
@@ -1,10 +1,13 @@
 import sys
 import rospy
 from time import sleep, time
+from math import sin, cos
 from pySerialTransfer import pySerialTransfer as txfer
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Quaternion
+from tf.broadcaster import TransformBroadcaster
 
 MAX_SPEED = 0.47      # meters/second
 BASE_WIDTH = 0.424    # meters, 16.675"
@@ -39,52 +42,117 @@ class RxPong:
 class RxOdometry:
     def __init__(self, link):
         self.link = link
+
+        # configure odometry publishing
         self.pub = rospy.Publisher('odom', Odometry, queue_size=1)
-        self.odom = Odometry()
-        self.sequence = 0
-        self.log_rate = 50
+        self.odom = Odometry(header=rospy.Header(frame_id="odom"), 
+                        child_frame_id='base_link')
+        # Note: the following covariance matrices are made up out of thin air
+        ODOM_POSE_COVARIANCE = [1e-3, 0, 0, 0, 0, 0,
+                                0, 1e-3, 0, 0, 0, 0,
+                                0, 0, 1e6, 0, 0, 0,
+                                0, 0, 0, 1e6, 0, 0,
+                                0, 0, 0, 0, 1e6, 0,
+                                0, 0, 0, 1e6, 0, 1e3]
+        ODOM_TWIST_COVARIANCE = [1e-3, 0, 0, 0, 0, 0,
+                                0, 1e-3, 0, 0, 0, 0,
+                                0, 0, 1e6, 0, 0, 0,
+                                0, 0, 0, 1e6, 0, 0,
+                                0, 0, 0, 0, 1e6, 0,
+                                0, 0, 0, 0, 0, 1e3]
+        self.odom.pose.covariance = ODOM_POSE_COVARIANCE
+        self.odom.twist.covariance = ODOM_TWIST_COVARIANCE
+
+        # configure tf publishing
+        self.tf_frame = rospy.get_param('~tf_frame', "odom")
+        if self.tf_frame != "":
+            self.odomBroadcaster = TransformBroadcaster()
+            rospy.loginfo('Publishing tf frame: %s' % self.tf_frame)
+
+        self.ros_odom_seq = 0
+        self.last_esp_seq = 0
+
+        self.log_rate = 1000    # print a log message every log_rate OdometryMsg's
         self.log_cnt = 0
 
     def handle_odometry(self):
         # rospy.logdebug ('Odometry callback got msg length: {}'.format(self.link.bytesRead))
-        self.odom.header.seq = self.sequence
-        self.sequence += 1
+        self.odom.header.seq = self.ros_odom_seq
+        self.ros_odom_seq += 1
         self.odom.header.stamp = rospy.Time.now()
-        self.odom.header.frame_id = 'base_link'
         rec_size = 0
+
+        # populate odom from OdometryMsg
+        # member seq
+        sequence = self.link.rx_obj(obj_type='I', start_pos=rec_size)
+        rec_size += txfer.STRUCT_FORMAT_LENGTHS['I']
+        # member poseX_m
         self.odom.pose.pose.position.x = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member poseY_m
         self.odom.pose.pose.position.y = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member heading_rad
         heading_rad = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member speedX_mps
         self.odom.twist.twist.linear.x = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member speedY_mps
         self.odom.twist.twist.linear.y = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member linear_speed_mps
         linear_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
-        angular_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
+        # member angular_speed_rps
+        self.odom.twist.twist.angular.z = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member odometer_m
         odometer = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member leftSpeed
         left_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member rightSpeed
         right_speed = self.link.rx_obj(obj_type='f', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['f']
+        # member leftEncoderCount
         left_enc_cnt = self.link.rx_obj(obj_type='I', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['I']
+        # member rightEncoderCount
         right_enc_cnt = self.link.rx_obj(obj_type='I', start_pos=rec_size)
         rec_size += txfer.STRUCT_FORMAT_LENGTHS['I']
 
+        # populate pose heading quaternion
+        quaternion = Quaternion()
+        quaternion.z = sin(heading_rad/2.0)
+        quaternion.w = cos(heading_rad/2.0)
+        self.odom.pose.pose.orientation = quaternion
+
+        # publish to /odom
         self.pub.publish(self.odom)
 
+        # publish to /tf
+        if self.tf_frame != "":
+            self.odomBroadcaster.sendTransform( (
+                                        self.odom.pose.pose.position.x,
+                                        self.odom.pose.pose.position.y,
+                                        self.odom.pose.pose.position.z), 
+                    (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
+                    rospy.Time(),
+                    "base_link",
+                    self.tf_frame )
+
+        # print odometry data to logs now and then
         self.log_cnt += 1
         if (self.log_cnt == self.log_rate):
             self.log_cnt = 0
             rospy.loginfo("poseX: {:.2f} poseY: {:.2f} heading: {:.2f} speed {:.2f}, odom: {:.2f}".format(
                 self.odom.pose.pose.position.x, self.odom.pose.pose.position.y, heading_rad, linear_speed, odometer))
 
+        if sequence != (self.last_esp_seq + 1):
+            rospy.logerr("Detected {} dropped odom messages".format(sequence - self.last_esp_seq + 1))
+        self.last_esp_seq = sequence
 
 class RxLog:
     def __init__(self, link):
@@ -125,27 +193,6 @@ class TxDriveMotorsRqst:
         self.seq = 0    # incrementing sequence # enables detecting dropped messages
         self.posted_seq = 0
         rospy.Subscriber("/cmd_vel", Twist, self.callback, queue_size=1)
-
-    def unused(self):
-        # This unused method is to hold conversion algorithm from  m/s & rad/s to left/right percent
-        """ Transform robot m/sec, rad/sec into left/right wheel speeds in m/s """
-        vel = cmd_vel.linear.x
-        delta_vel = cmd_vel.angular.z * (BASE_WIDTH / 2.0)
-        # clamp max wheel speed rqst to +/- MAX_SPEED and ratio vel, delta_vel accordingly
-        k = max(abs(vel-delta_vel),abs(vel+delta_vel))
-        if (k > MAX_SPEED):
-            vel = vel * MAX_SPEED / k
-            delta_vel = delta_vel * MAX_SPEED / k
-            rospy.logwarn_throttle(1.0, "Requested speed > MAX_SPEED, throttling speed")
-        wheel_speeds = [ (vel - delta_vel), (vel + delta_vel) ]
-        left_wheel_pct = int((wheel_speeds[0] / MAX_SPEED) * 100)
-        right_wheel_pct = int((wheel_speeds[1] / MAX_SPEED) * 100)
-        if abs(left_wheel_pct) > 100 or abs(right_wheel_pct) > 100:
-            rospy.logerr("requested drive power > 100")
-        rospy.logdebug_throttle(
-            1.0,
-            "cmd_vel (linear/ang): {:.2f}, {:.2f} produced wheel_speeds (left/right): {:.2f} {:.2f}, drive pct: {} {}".format(
-            cmd_vel.linear.x, cmd_vel.angular.z, wheel_speeds[0], wheel_speeds[1], left_wheel_pct, right_wheel_pct))
 
     def callback(self, cmd_vel):
         # drop requests received less than 50ms since the last one
