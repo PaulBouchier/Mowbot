b8c52d15e9cd5e167a5aff3fad8da37fa0cb2aea
Paul Bouchier <paul.bouchier@gmail.com>
2023-11-11 01:18:42 +1300

    enable stop.py to pause

--- mowbot_bringup/scripts/stop.py
+++ mowbot_bringup/scripts/stop.py
@@ -7,70 +7,54 @@ from geometry_msgs.msg import Twist, Point, Quaternion
 from mowbot_msgs.msg import OdomExtra, PlatformData
 import tf
 from math import radians, copysign, sqrt, pow, pi, asin, atan2
+from MoveParent import MoveParent
 
 loop_rate = 10       # loop rate
-speed = 0.25    # driving speed, fwd or back
-vel_slew_rate = 0.5 / loop_rate  # m/s^2 per loop
-rot_slew_rate = 1.0 / loop_rate  # rad/s^2 per loop
 
-class Stop():
+class Stop(MoveParent):
     def __init__(self, cmd_vel):
-        self.once = True
-        self.cmd_vel = cmd_vel      # cmd_vel publisher
-
-        # subscribers to robot data
-        rospy.Subscriber("/odom_extra", OdomExtra, self.odom_callback, queue_size=1)
-        rospy.Subscriber("/platform_data", PlatformData, self.platform_callback, queue_size=1)
-        self.odom_extra = OdomExtra()
-        self.platform_data = PlatformData()
-
-        self.move_cmd = Twist()
-        time.sleep(0.1)
+        super().__init__(cmd_vel)
+        self.pause = 0            # pause after stop, in seconds
+        self.end_pause_time = rospy.Time.now() # time when pause ends
 
     def parse_argv(self, argv):
+        # check whether a pause argument was supplied
+        if (len(argv) > 0):
+            try:
+                pause_arg = float(argv[0])
+                self.pause = pause_arg
+                self.end_pause_time = rospy.Time.now() + rospy.Duration(self.pause)  # time when pause ends
+                print('Using supplied pause {}'.format(self.pause))
+                return 1
+            except ValueError:
+                return 0
         return 0            # return number of args consumed
 
     def print(self):
-        print('Stop')
+        print('Stop and pause for {} seconds'.format(self.pause))
 
     def run(self):
         if self.once:
             start_odom = self.odom_extra.odometer
             rospy.loginfo('Stop: start odometer: {}, heading: {}'.format(self.odom_extra.odometer, self.odom_extra.heading))
+            self.once = False
 
         # loop sending stop commands until both linear & angular request stopped
         if (abs(self.platform_data.commandedLinear) < 0.01 and abs(self.platform_data.commandedAngular) < 0.01):
-            return True
+            self.cmd_vel.publish(Twist())   # stop the robot
+            if rospy.Time.now() > self.end_pause_time:
+                rospy.loginfo('Stop: paused for {} seconds'.format(self.pause))
+                return True
+            else:
+                return False
 
         self.move_cmd.linear.x = self.slew_vel(0)
         self.move_cmd.angular.z = self.slew_rot(0)
 
         # rospy.loginfo(self.move_cmd.linear.x)
         self.cmd_vel.publish(self.move_cmd)
-        self.r.sleep()
 
-        self.cmd_vel.publish(Twist())   # stop the robot
         return False
-
-    def slew_vel(self, to):
-        return self.slew(self.platform_data.commandedLinear, 0.0, vel_slew_rate)
-
-    def slew_rot(self, to):
-        return self.slew(self.platform_data.commandedAngular, 0, rot_slew_rate)
-
-    def slew(self, current, to, slew_rate):
-        diff = to - current
-        if diff > slew_rate:
-            return current + slew_rate
-        if diff < -slew_rate:
-            return current - slew_rate
-        return to
-
-    def odom_callback(self, odom_extra_msg):
-        self.odom_extra = odom_extra_msg
-
-    def platform_callback(self, platform_data_msg):
-        self.platform_data = platform_data_msg
 
 def shutdown():
     # Always stop the robot when shutting down the node.
@@ -79,7 +63,7 @@ def shutdown():
     rospy.sleep(1)
 
 def usage():
-    print('Usage: stop.py - ramp down linear & rotational speed')
+    print('Usage: stop.py [pause] - ramp down linear & rotational speed & pause if pause is not zero')
     sys.exit()
 
 if __name__ == '__main__':
@@ -97,6 +81,7 @@ if __name__ == '__main__':
 
     try:
         m = Stop(cmd_vel)
+        argv_index += m.parse_argv(sys.argv[argv_index:])
         m.print()
         while (not rospy.is_shutdown()):
             if m.run():
