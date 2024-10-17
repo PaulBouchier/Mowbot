47e281f3ab092fa41e9d9582c9b87f04efebb6f5
Paul Bouchier <paul.bouchier@gmail.com>
2024-10-17 05:17:01 -0500

    xbox controller works

--- robot_hardware/setup.py
+++ robot_hardware/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'robot_hardware'
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # include all files in config
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
